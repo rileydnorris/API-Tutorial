0fe4f944ac32692e642b4906f2d84769c064bda4
Riley Norris <rileydnorris@gmail.com>
2018-12-30 00:55:36 -0600

    Initial Commit

---
+++ API-Tutorial-Video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API-Tutorial-Video
+//
+//  Created by Riley Norris on 12/30/18.
+//  Copyright © 2018 Riley Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ API-Tutorial-VideoTests/API_Tutorial_VideoTests.swift
@@ -0,0 +1,34 @@
+//
+//  API_Tutorial_VideoTests.swift
+//  API-Tutorial-VideoTests
+//
+//  Created by Riley Norris on 12/30/18.
+//  Copyright © 2018 Riley Norris. All rights reserved.
+//
+
+import XCTest
+@testable import API_Tutorial_Video
+
+class API_Tutorial_VideoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
